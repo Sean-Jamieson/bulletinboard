68db89669d38ce287987976d53cdd7ee55693b22
sean jamieson <sjami273@mtryoal.ca>
2022-02-19 12:22:27 -0700

    quick push

--- server/index.js
+++ server/index.js
@@ -2,3 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+const app = express();
+const port = process.env.PORT || 3000;
+
+dotenv.config();
+
+app.listen(port, () =>{
+    console.log(`listening on port ${port}`);
+})
