a1ca4df21493b9553f6d539957a3892d42b7db15
Matthieu Bardal <mattbardal@gmail.com>
2023-01-17 19:23:38 -0700

    update API_URL

--- client/src/api/asyncFunctions.ts
+++ client/src/api/asyncFunctions.ts
@@ -1,6 +1,6 @@
 import { CreateInput } from "../hooks/useCreateService";
 
-const API_URL = "https://bltnbrd.herokuapp.com/api/";
+const API_URL = "https://pewter-thoracic-cushion.glitch.me/";
 
 /* postData(url, data)
  *
